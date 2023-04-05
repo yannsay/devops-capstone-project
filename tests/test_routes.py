db7667a32a8f13ac83dc95cb19e34405386831f1
Yann Say <yann.say@hotmail.com>
2023-04-05 09:41:52 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -190,8 +190,8 @@ class TestAccountService(TestCase):
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)    
-    
+            self.assertEqual(response.headers.get(key), value)
+
     ################
     # CORS headers #
     ################
